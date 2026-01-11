da5cc579caaa2fb250d6af3ac35018066d5edd00
Pierre Thabet <pierre.thabet@outlook.com>
2026-01-11 09:35:12 +0100

    Create Alien, Laptop and FirstprojApplication class.

--- firstproj/src/main/java/com/pierre/firstproj/FirstprojApplication.java
+++ firstproj/src/main/java/com/pierre/firstproj/FirstprojApplication.java
@@ -2,12 +2,18 @@ package com.pierre.firstproj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class FirstprojApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FirstprojApplication.class, args);
+		ApplicationContext context = SpringApplication.run(FirstprojApplication.class, args);
+
+		Alien obj = context.getBean(Alien.class);
+		obj.code();
+
+
 	}
 
 }
