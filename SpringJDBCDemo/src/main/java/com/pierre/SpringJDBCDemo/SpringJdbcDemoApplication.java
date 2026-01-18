7bdb5b13e4cab756b7b7618f0dabbb37cf0d6b9a
Pierre Thabet <pierre.thabet@outlook.com>
2026-01-18 17:13:22 +0100

    Make use of JDBC in Spring.

--- SpringJDBCDemo/src/main/java/com/pierre/SpringJDBCDemo/SpringJdbcDemoApplication.java
+++ SpringJDBCDemo/src/main/java/com/pierre/SpringJDBCDemo/SpringJdbcDemoApplication.java
@@ -1,13 +1,33 @@
 package com.pierre.SpringJDBCDemo;
 
+import com.pierre.SpringJDBCDemo.model.Alien;
+import com.pierre.SpringJDBCDemo.repo.AlienRepo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringJdbcDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringJdbcDemoApplication.class, args);
+
+
+
+		ApplicationContext context = SpringApplication.run(SpringJdbcDemoApplication.class, args);
+
+		Alien alien1 = context.getBean(Alien.class);
+		alien1.setId(111);
+		alien1.setName("Pierre");
+		alien1.setTech("Java");
+
+		AlienRepo repo = context.getBean(AlienRepo.class);
+		repo.save(alien1);
+
+		System.out.println(repo.findAll());
+
+
 	}
+
+
 
 }
---
+++ SpringJDBCDemo/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into alien (id, name, tech) values (101, 'tita', 'swift');
+insert into alien (id, name, tech) values (102, 'mita', 'AI');
+insert into alien (id, name, tech) values (103, 'lita', 'pofta');
